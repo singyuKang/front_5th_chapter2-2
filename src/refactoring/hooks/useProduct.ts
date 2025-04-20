5883421e732336d80c193d947fb59a06131c4d54
singyuKang <rkdtlscks123@naver.com>
2025-04-20 20:27:43 +0900

    feat : 상품 추가 기능

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -3,8 +3,7 @@ import { Product } from '../../types.ts';
 
 //  ✓ 특정 제품으로 초기화할 수 있다.
 //  ✓ 제품을 업데이트할 수 있다.
-//  × 새로운 제품을 추가할 수 있다.
-
+//  ✓ 새로운 제품을 추가할 수 있다.
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState(initialProducts);
 
@@ -14,7 +13,9 @@ export const useProducts = (initialProducts: Product[]) => {
     );
   };
 
-  const addProduct = () => {};
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
 
   return {
     products,
