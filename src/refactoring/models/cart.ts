f1dae68a42049fd1d00f464be59b759d2f5cef6b
singyuKang <rkdtlscks123@naver.com>
2025-04-20 21:23:26 +0900

    feat : updateCartItemQuantity 함수 추가

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -41,5 +41,16 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number,
 ): CartItem[] => {
-  return [];
+  const updateCart = cart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0 ? { ...item, quantity: updatedQuantity } : null;
+      }
+      return item;
+    })
+    .filter((item): item is CartItem => item !== null);
+
+  return updateCart;
 };
