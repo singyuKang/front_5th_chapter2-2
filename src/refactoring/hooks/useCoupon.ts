cdc2c0549f8bd5ed10dee6187f0f19cba0ea5dbb
singyuKang <rkdtlscks123@naver.com>
2025-04-20 19:14:11 +0900

    feat : products, coupons 초기값 세팅

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,7 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  return { coupons, addCoupon: () => undefined };
 };
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
   return {
-    products: [],
+    products,
     updateProduct: () => undefined,
     addProduct: () => undefined,
   };
