1bc99d65d7acc73dc345b76b6e9d07545ae7ef52
singyuKang <rkdtlscks123@naver.com>
2025-04-20 20:25:15 +0900

    feat : 상품 update 기능 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
+
+//  ✓ 특정 제품으로 초기화할 수 있다.
+//  ✓ 제품을 업데이트할 수 있다.
+//  × 새로운 제품을 추가할 수 있다.
 
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState(initialProducts);
 
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)),
+    );
+  };
+
+  const addProduct = () => {};
+
   return {
     products,
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    updateProduct,
+    addProduct,
   };
 };
